85f8ac21bcfe22cc4fb9d94deb95d4c27b7bc1f3
jbjog <jeanbaptiste.jogeix@outlook.fr>
2019-01-16 14:38:28 +0100

    add logo

--- core/src/com/noname/qn/QNGame.java
+++ core/src/com/noname/qn/QNGame.java
@@ -13,18 +13,18 @@ public class QNGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("QN.png");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img,0,0);
 		batch.end();
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
--- desktop/src/com/noname/qn/desktop/DesktopLauncher.java
+++ desktop/src/com/noname/qn/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package com.noname.qn.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.graphics.Color;
 import com.noname.qn.QNGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Quantic NightMare";
+		config.width = 721;
+		config.height = 386;
 		new LwjglApplication(new QNGame(), config);
 	}
 }
