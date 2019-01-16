8e14cbc8e9f00b21894f825e926475d1519a0b6f
jbjog <jeanbaptiste.jogeix@outlook.fr>
2019-01-16 11:36:55 +0100

    first commit

---
+++ desktop/src/com/noname/qn/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.noname.qn.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.noname.qn.QNGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QNGame(), config);
+	}
+}
