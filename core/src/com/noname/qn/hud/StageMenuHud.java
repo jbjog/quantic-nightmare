8d0d088e7584431a46bdf2e76edc57a24dbd105f
sirrhyl <sirrhyl@gmail.com>
2019-01-18 16:35:54 +0100

    feat: add button to hud stage

--- core/src/com/noname/qn/hud/StageMenuHud.java
+++ core/src/com/noname/qn/hud/StageMenuHud.java
@@ -3,13 +3,17 @@ package com.noname.qn.hud;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Disposable;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -22,6 +26,11 @@ public class StageMenuHud implements Disposable {
     private Viewport viewport;
     private BitmapFont font;
 
+    private Texture myTexture;
+    private TextureRegion myTextureRegion;
+    private TextureRegionDrawable myTexRegionDrawable;
+    private ImageButton button;
+
     private Label.LabelStyle redStyle;
     private Label.LabelStyle whiteStyle;
     private Label titleLabel;
@@ -33,6 +42,11 @@ public class StageMenuHud implements Disposable {
         stage = new Stage(viewport, game.batch);
         Gdx.input.setInputProcessor(stage);
 
+        myTexture = new Texture(Gdx.files.internal("1.png"));
+        myTextureRegion = new TextureRegion(myTexture);
+        myTexRegionDrawable = new TextureRegionDrawable(myTextureRegion);
+        button = new ImageButton(myTexRegionDrawable);
+
         font = this.getFont();
         redStyle = new Label.LabelStyle(font, Color.RED);
         whiteStyle = new Label.LabelStyle(font, Color.WHITE);
@@ -40,12 +54,15 @@ public class StageMenuHud implements Disposable {
         Table table = new Table();
         table.top();
         table.setFillParent(true);
+
         stage.addActor(table);
 
         titleLabel = new Label("Choose your Nightmare", whiteStyle);
         backLabel = new Label("Back", whiteStyle);
 
         table.add(titleLabel).width(275).height(200);
+        table.row();
+        table.add(button).size(40f, 40f);
         table.row();
         table.add(backLabel);
 
