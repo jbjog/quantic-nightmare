eca23fb26fee092820c2f569a1db9442ebb234df
acydcytryk <acydcytryk@gmail.com>
2019-06-18 17:16:07 +0200

    ajout SplashScreen en cours

--- core/src/com/noname/qn/screen/QNScreen.java
+++ core/src/com/noname/qn/screen/QNScreen.java
@@ -4,6 +4,9 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.noname.qn.hud.QNHud;
 import com.noname.qn.service.gui.Gamable;
 import com.noname.qn.service.gui.Gamer;
@@ -12,6 +15,9 @@ public class QNScreen implements Screen, Gamer {
     private final Gamable game;
     protected OrthographicCamera camera;
     protected QNHud hud;
+    private SpriteBatch batch;
+    private Sprite splash;
+
 
     public QNScreen(final Gamable game) {
         this.game = game;
@@ -24,6 +30,10 @@ public class QNScreen implements Screen, Gamer {
         Gdx.gl.glClearColor(0.0f, 0.0f, 0.0f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        batch.begin();
+        splash.draw(batch);
+        batch.end();
+
         camera.update();
 
         game.getBatch().begin();
@@ -35,6 +45,11 @@ public class QNScreen implements Screen, Gamer {
 
     @Override
     public void show() {
+        batch = new SpriteBatch();
+
+        Texture splashtexture = new Texture("no_name_studio.jpg");
+        splash = new Sprite(splashtexture);
+        splash.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
     }
     @Override
@@ -55,6 +70,8 @@ public class QNScreen implements Screen, Gamer {
     }
     @Override
     public void dispose() {
+        batch.dispose();
+        splash.getTexture().dispose();
 
     }
 
